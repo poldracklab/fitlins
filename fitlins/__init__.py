0791c7e3d4e90616503f8b7bbeac2bed2c715b4c
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-01-24 15:33:04 -0500

    Add __init__.py files

---
+++ fitlins/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+from .info import (
+    __version__,
+    __author__,
+    __copyright__,
+    __credits__,
+    __license__,
+    __maintainer__,
+    __email__,
+    __status__,
+    __url__,
+    __packagename__,
+    __description__,
+    __longdesc__
+)
