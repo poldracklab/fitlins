fbd5fcc168d8014519fbc0c958321ca326d65516
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-03-01 14:55:20 -0500

    RF: Remove currently unneeded arguments, simplify organization

--- fitlins/cli/run.py
+++ fitlins/cli/run.py
@@ -11,12 +11,14 @@ import os
 import os.path as op
 import time
 import logging
-import uuid
 import warnings
 from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter
-from multiprocessing import cpu_count
-from time import strftime
+
+from .. import info
+from ..utils import bids
+from ..base import init, first_level, second_level
+from ..viz.reports import write_report
 
 logging.addLevelName(25, 'INFO')  # Add a new level between INFO and WARNING
 logger = logging.getLogger('cli')
@@ -25,7 +27,6 @@ logger.setLevel(25)
 INIT_MSG = """
 Running FitLins version {version}:
   * Participant list: {subject_list}.
-  * Run identifier: {uuid}.
 """.format
 
 
@@ -33,11 +34,21 @@ def _warn_redirect(message, category, filename, lineno, file=None, line=None):
     logger.warning('Captured warning (%s): %s', category, message)
 
 
+def default_path(arg, base_dir, default_name):
+    """Generate absolute path from absolute, relative, or missing path
+
+    Missing paths are given a default value, which may be absolute or relative,
+    and relative paths are resolved relative to a base directory.
+    """
+    path = default_name if arg is None else arg
+    if not op.isabs(path):
+        path = op.abspath(op.join(base_dir, path))
+    return path
+
+
 def get_parser():
     """Build parser object"""
-    from fitlins.info import __version__
-
-    verstr = 'fitlins v{}'.format(__version__)
+    verstr = 'fitlins v{}'.format(info.__version__)
 
     parser = ArgumentParser(description='FitLins: Workflows for Fitting Linear models to fMRI',
                             formatter_class=RawTextHelpFormatter)
@@ -58,13 +69,9 @@ def get_parser():
     parser.add_argument('-v', '--version', action='version', version=verstr)
 
     g_bids = parser.add_argument_group('Options for filtering BIDS queries')
-    g_bids.add_argument('--participant_label', '--participant-label', action='store', nargs='+',
+    g_bids.add_argument('--participant-label', action='store', nargs='+',
                         help='one or more participant identifiers (the sub- prefix can be '
                              'removed)')
-    g_bids.add_argument('-s', '--session-id', action='store', default='single_session',
-                        help='select a specific session to be processed')
-    g_bids.add_argument('-t', '--task-id', action='store',
-                        help='select a specific task to be processed')
     g_bids.add_argument('-m', '--model', action='store', default='model.json',
                         help='location of BIDS model description (default bids_dir/model.json)')
     g_bids.add_argument('-p', '--preproc-dir', action='store', default='fmriprep',
@@ -76,28 +83,6 @@ def get_parser():
     g_perfm = parser.add_argument_group('Options to handle performance')
     g_perfm.add_argument('--debug', action='store_true', default=False,
                          help='run debug version of workflow')
-    g_perfm.add_argument('--nthreads', '--n_cpus', '-n-cpus', action='store', default=0, type=int,
-                         help='maximum number of threads across all processes')
-    g_perfm.add_argument('--omp-nthreads', action='store', type=int, default=0,
-                         help='maximum number of threads per-process')
-    g_perfm.add_argument('--mem_mb', '--mem-mb', action='store', default=0, type=int,
-                         help='upper bound memory limit for FMRIPREP processes')
-    g_perfm.add_argument('--use-plugin', action='store', default=None,
-                         help='nipype plugin configuration file')
-
-    g_other = parser.add_argument_group('Other options')
-    g_other.add_argument('-w', '--work-dir', action='store', default='work',
-                         help='path where intermediate results should be stored')
-    g_other.add_argument(
-        '--reports-only', action='store_true', default=False,
-        help='only generate reports, don\'t run workflows. This will only rerun report '
-             'aggregation, not reportlet generation for specific nodes.')
-    g_other.add_argument(
-        '--run-uuid', action='store', default=None,
-        help='Specify UUID of previous run, to include error logs in report. '
-             'No effect without --reports-only.')
-    g_other.add_argument('--write-graph', action='store_true', default=False,
-                         help='Write workflow graph.')
 
     return parser
 
@@ -114,145 +99,55 @@ def main(args=None):
 
 def create_workflow(opts):
     """Build workflow"""
-    from niworkflows.nipype import config as ncfg
-    from niworkflows.nipype.pipeline import engine as pe
-    from fitlins.info import __version__
-    from fitlins.utils.bids import collect_participants
-    from fitlins.base import init, first_level, second_level
-    from fitlins.viz.reports import write_report
-
-    run_context = {'version': __version__,
-                   'command': ' '.join(sys.argv),
-                   'timestamp': time.strftime('%Y-%m-%d %H:%M:%S %z'),
-                   }
-
-    # Set up some instrumental utilities
-    errno = 0
-    run_uuid = strftime('%Y%m%d-%H%M%S_') + str(uuid.uuid4())
 
     # First check that bids_dir looks like a BIDS folder
     bids_dir = op.abspath(opts.bids_dir)
-    subject_list = collect_participants(
+    subject_list = bids.collect_participants(
         bids_dir, participant_label=opts.participant_label)
 
-    # Nipype plugin configuration
-    plugin_settings = {'plugin': 'Linear'}
-    nthreads = opts.nthreads
-    if opts.use_plugin is not None:
-        from yaml import load as loadyml
-        with open(opts.use_plugin) as f:
-            plugin_settings = loadyml(f)
-    else:
-        # Setup multiprocessing
-        nthreads = opts.nthreads
-        if nthreads == 0:
-            nthreads = cpu_count()
-
-        if nthreads > 1:
-            plugin_settings['plugin'] = 'MultiProc'
-            plugin_settings['plugin_args'] = {'n_procs': nthreads}
-            if opts.mem_mb:
-                plugin_settings['plugin_args']['memory_gb'] = opts.mem_mb / 1024
-
-    omp_nthreads = opts.omp_nthreads
-    if omp_nthreads == 0:
-        omp_nthreads = min(nthreads - 1 if nthreads > 1 else cpu_count(), 8)
-
-    if 1 < nthreads < omp_nthreads:
-        raise RuntimeError(
-            'Per-process threads (--omp-nthreads={:d}) cannot exceed total '
-            'threads (--nthreads/--n_cpus={:d})'.format(omp_nthreads, nthreads))
-
-    # Set up directories
     output_dir = op.abspath(opts.output_dir)
-    log_dir = op.join(output_dir, 'fitlins', 'logs')
-    work_dir = op.abspath(opts.work_dir)
-
-    # Check and create output and working directories
     os.makedirs(output_dir, exist_ok=True)
-    os.makedirs(log_dir, exist_ok=True)
-    os.makedirs(work_dir, exist_ok=True)
-
-    # Nipype config (logs and execution)
-    ncfg.update_config({
-        'logging': {'log_directory': log_dir, 'log_to_file': True},
-        'execution': {'crashdump_dir': log_dir, 'crashfile_format': 'txt'},
-    })
-
-    # Called with reports only
-    if opts.reports_only:
-        logger.log(25, 'Running --reports-only on participants %s', ', '.join(subject_list))
-        if opts.run_uuid is not None:
-            run_uuid = opts.run_uuid
-        # report_errors = [
-        #     run_reports(op.join(work_dir, 'reportlets'), output_dir, subject_label,
-        #                 run_uuid=run_uuid)
-        #     for subject_label in subject_list]
-        # sys.exit(int(sum(report_errors) > 0))
-        sys.exit(0)
 
     # Build main workflow
     logger.log(25, INIT_MSG(
-        version=__version__,
-        subject_list=subject_list,
-        uuid=run_uuid)
+        version=info.__version__,
+        subject_list=subject_list)
     )
 
-    model = opts.model
-    if model is None:
-        model = 'model.json'
-    if not op.isabs(model):
-        model = op.join(bids_dir, model)
-
-    preproc_dir = opts.preproc_dir
-    if preproc_dir is None:
-        preproc_dir = 'fmriprep'
-    if not op.isabs(preproc_dir):
-        preproc_dir = op.join(output_dir, preproc_dir)
-
+    model = default_path(opts.model, bids_dir, 'model.json')
+    preproc_dir = default_path(opts.preproc_dir, output_dir, 'fmriprep')
     deriv_dir = op.join(output_dir, 'fitlins')
 
+    # BIDS-Apps prefers 'participant', BIDS-Model prefers 'subject'
     level = 'subject' if opts.analysis_level == 'participant' else opts.analysis_level
+
+    try:
+        retcode = run_model(model, level, bids_dir, preproc_dir, deriv_dir)
+    except Exception:
+        retcode = 1
+
+    sys.exit(retcode)
+
+
+def run_model(model, target_level, bids_dir, preproc_dir, deriv_dir):
+    run_context = {'version': info.__version__,
+                   'command': ' '.join(sys.argv),
+                   'timestamp': time.strftime('%Y-%m-%d %H:%M:%S %z'),
+                   }
 
     analysis = init(model, bids_dir, preproc_dir)
     report_dicts = first_level(analysis, analysis.blocks[0], deriv_dir)
     write_report(analysis.blocks[0].level, report_dicts, run_context,
                  deriv_dir)
-    if analysis.blocks[0].level == opts.analysis_level:
-        sys.exit(0)
+    if analysis.blocks[0].level == target_level:
+        return 0
     for block in analysis.blocks[1:]:
         report_dicts = second_level(analysis, block, deriv_dir)
         write_report(block.level, report_dicts, run_context, deriv_dir)
-        if block.level == level:
+        if block.level == target_level:
             break
 
-    sys.exit(0)
-
-    fitlins_wf = pe.Workflow(name='fitlins_wf')
-
-    if opts.write_graph:
-        fitlins_wf.write_graph(graph2use="colored", format='svg', simple_form=True)
-
-    try:
-        fitlins_wf.run(**plugin_settings)
-    except RuntimeError as e:
-        if "Workflow did not execute cleanly" in str(e):
-            errno = 1
-        else:
-            raise(e)
-
-    # Generate reports phase
-    # report_errors = [run_reports(
-    #     op.join(work_dir, 'reportlets'), output_dir, subject_label, run_uuid=run_uuid)
-    #     for subject_label in subject_list]
-
-    # if sum(report_errors):
-    #     logger.warning('Errors occurred while generating reports for participants: %s.',
-    #                    ', '.join(['%s (%d)' % (subid, err)
-    #                               for subid, err in zip(subject_list, report_errors)]))
-
-    # errno += sum(report_errors)
-    sys.exit(int(errno > 0))
+    return 0
 
 
 if __name__ == '__main__':
