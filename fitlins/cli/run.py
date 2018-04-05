f5658d9e05663fbd16176bd0c58fcdc2607b93fb
adelavega <delavega@utexas.edu>
2018-04-05 17:42:28 -0500

    Separate CLI and run behavior

--- fitlins/cli/run.py
+++ fitlins/cli/run.py
@@ -86,16 +86,23 @@ def get_parser():
 
     return parser
 
-
 def main(args=None):
     """Entry point"""
     warnings.showwarning = _warn_redirect
+
+    try:
+        retcode = create_workflow(args)
+    except Exception as e:
+        retcode= 1
+
+    sys.exit(retcode)
+
+def run(args):
     opts = get_parser().parse_args(args)
     if opts.debug:
         logger.setLevel(logging.DEBUG)
 
     create_workflow(opts)
-
 
 def create_workflow(opts):
     """Build workflow"""
@@ -121,13 +128,9 @@ def create_workflow(opts):
     # BIDS-Apps prefers 'participant', BIDS-Model prefers 'subject'
     level = 'subject' if opts.analysis_level == 'participant' else opts.analysis_level
 
-    try:
-        retcode = run_model(model, opts.space, level, bids_dir, preproc_dir,
-                            deriv_dir)
-    except Exception:
-        retcode = 1
+    return run_model(model, opts.space, level, bids_dir, preproc_dir,
+                        deriv_dir)
 
-    sys.exit(retcode)
 
 
 def run_model(model, space, target_level, bids_dir, preproc_dir, deriv_dir):
