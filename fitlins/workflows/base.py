fde212e330107384ac3f26cdbecbc4c06fd2fc33
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-04-13 14:59:49 -0400

    ENH: Sink all images

--- fitlins/workflows/base.py
+++ fitlins/workflows/base.py
@@ -3,7 +3,8 @@ from ..interfaces.bids import LoadLevel1BIDSModel, BIDSSelect, BIDSDataSink
 from ..interfaces.nistats import FirstLevelModel
 
 
-def init_fitlins_wf(bids_dir, preproc_dir, base_dir, name='fitlins_wf'):
+def init_fitlins_wf(bids_dir, preproc_dir, out_dir,
+                    base_dir=None, name='fitlins_wf'):
     wf = pe.Workflow(name=name, base_dir=base_dir)
 
     loader = pe.Node(
@@ -19,15 +20,26 @@ def init_fitlins_wf(bids_dir, preproc_dir, base_dir, name='fitlins_wf'):
         iterfield=['session_info', 'contrast_info', 'bold_file', 'mask_file'],
         name='flm')
 
-    sink_dms = pe.MapNode(
-        BIDSDataSink(
-            base_directory='/work/derivatives/fitlins',
-            fixed_entities={'type': 'design'},
-            path_patterns='sub-{subject}/[ses-{session}/]sub-{subject}_'
-                          '[ses-{session}_]task-{task}_bold_{type<design>}.svg'
-            ),
+    image_pattern = 'sub-{subject}/[ses-{session}/]sub-{subject}_' \
+        '[ses-{session}_]task-{task}_bold_{type<design|corr|contrasts>}.svg'
+    ds_design = pe.MapNode(
+        BIDSDataSink(base_directory=out_dir, fixed_entities={'type': 'design'},
+                     path_patterns=image_pattern),
         iterfield=['entities', 'in_file'],
-        name='sink_dms')
+        name='ds_design')
+
+    ds_corr = pe.MapNode(
+        BIDSDataSink(base_directory=out_dir, fixed_entities={'type': 'corr'},
+                     path_patterns=image_pattern),
+        iterfield=['entities', 'in_file'],
+        name='ds_corr')
+
+    ds_contrasts = pe.MapNode(
+        BIDSDataSink(base_directory=out_dir,
+                     fixed_entities={'type': 'contrasts'},
+                     path_patterns=image_pattern),
+        iterfield=['entities', 'in_file'],
+        name='ds_contrasts')
 
     def _get_ents(session_infos):
         return [info['entities'] for info in session_infos]
@@ -38,8 +50,12 @@ def init_fitlins_wf(bids_dir, preproc_dir, base_dir, name='fitlins_wf'):
                        ('contrast_info', 'contrast_info')]),
         (getter, flm, [('bold_files', 'bold_file'),
                        ('mask_files', 'mask_file')]),
-        (loader, sink_dms, [(('session_info', _get_ents), 'entities')]),
-        (flm, sink_dms, [('design_matrix_plot', 'in_file')]),
+        (loader, ds_design, [(('session_info', _get_ents), 'entities')]),
+        (loader, ds_corr, [(('session_info', _get_ents), 'entities')]),
+        (loader, ds_contrasts, [(('session_info', _get_ents), 'entities')]),
+        (flm, ds_design, [('design_matrix_plot', 'in_file')]),
+        (flm, ds_corr, [('correlation_matrix_plot', 'in_file')]),
+        (flm, ds_contrasts, [('contrast_matrix_plot', 'in_file')]),
         ])
 
     return wf
