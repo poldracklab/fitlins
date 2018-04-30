32f7d80bac9b54151b426dcbe65f7df2bdbca98d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-04-30 19:19:09 -0400

    RF: Permit multiple models to be specified

--- fitlins/interfaces/bids.py
+++ fitlins/interfaces/bids.py
@@ -61,7 +61,7 @@ class LoadLevel1BIDSModelInputSpec(BaseInterfaceInputSpec):
     bids_dirs = InputMultiPath(Directory(exists=True),
                                mandatory=True,
                                desc='BIDS dataset root directories')
-    model = traits.Either('default', File(exists=True),
+    model = traits.Either('default', InputMultiPath(File(exists=True)),
                           desc='Model filename')
     selectors = traits.Dict(desc='Limit collected sessions')
     include_pattern = InputMultiPath(
@@ -91,83 +91,80 @@ class LoadLevel1BIDSModel(SimpleInterface):
             exclude = None
         layout = gb.BIDSLayout(self.inputs.bids_dirs, include=include,
                                exclude=exclude)
-        model_fname = self.inputs.model
-        if not isdefined(model_fname):
+        models = self.inputs.model
+        if not isdefined(models):
             models = layout.get(type='model')
-            if len(models) == 1:
-                model_fname = models[0].filename
-            elif models:
-                raise ValueError("Ambiguous model")
-            else:
+            if not models:
                 raise ValueError("No models found")
-        elif model_fname == 'default':
-            model_fname = ba.auto_model(layout)
+        elif models == 'default':
+            models = ba.auto_model(layout)[0]
 
         selectors = (self.inputs.selectors
                      if isdefined(self.inputs.selectors) else {})
 
-        analysis = ba.Analysis(model=model_fname, layout=layout)
-        selectors.update(analysis.model['input'])
-        analysis.setup(**selectors)
-        block = analysis.blocks[0]
-
         entities = []
         session_info = []
         contrast_info = []
-        for paradigm, _, ents in block.get_design_matrix(
-                block.model['HRF_variables'], mode='sparse'):
-            info = {}
+        for model in models:
+            analysis = ba.Analysis(model=model, layout=layout)
+            selectors.update(analysis.model['input'])
+            analysis.setup(**selectors)
+            block = analysis.blocks[0]
 
-            bold_files = layout.get(type='bold',
-                                    extensions=['.nii', '.nii.gz'],
-                                    **ents)
-            if len(bold_files) != 1:
-                raise ValueError('Too many BOLD files found')
+            for paradigm, _, ents in block.get_design_matrix(
+                    block.model['HRF_variables'], mode='sparse'):
+                info = {}
 
-            fname = bold_files[0].filename
+                bold_files = layout.get(type='bold',
+                                        extensions=['.nii', '.nii.gz'],
+                                        **ents)
+                if len(bold_files) != 1:
+                    raise ValueError('Too many BOLD files found')
 
-            # Required field in seconds
-            TR = layout.get_metadata(fname)['RepetitionTime']
+                fname = bold_files[0].filename
 
-            _, confounds, _ = block.get_design_matrix(mode='dense',
-                                                      sampling_rate=1/TR,
-                                                      **ents)[0]
+                # Required field in seconds
+                TR = layout.get_metadata(fname)['RepetitionTime']
 
-            # Note that FMRIPREP includes CosineXX columns to accompany
-            # t/aCompCor
-            # We may want to add criteria to include HPF columns that are not
-            # explicitly listed in the model
-            names = [col for col in confounds.columns
-                     if col.startswith('NonSteadyStateOutlier') or
-                     col in block.model['variables']]
+                _, confounds, _ = block.get_design_matrix(mode='dense',
+                                                          sampling_rate=1/TR,
+                                                          **ents)[0]
 
-            ent_string = '_'.join('{}-{}'.format(key, val)
-                                  for key, val in ents.items())
-            events_file = os.path.join(runtime.cwd,
-                                       '{}_events.h5'.format(ent_string))
-            confounds_file = os.path.join(runtime.cwd,
-                                          '{}_confounds.h5'.format(ent_string))
-            paradigm.to_hdf(events_file, key='events')
-            confounds[names].fillna(0).to_hdf(confounds_file, key='confounds')
-            info['events'] = events_file
-            info['confounds'] = confounds_file
-            info['repetition_time'] = TR
+                # Note that FMRIPREP includes CosineXX columns to accompany
+                # t/aCompCor
+                # We may want to add criteria to include HPF columns that are not
+                # explicitly listed in the model
+                names = [col for col in confounds.columns
+                         if col.startswith('NonSteadyStateOutlier') or
+                         col in block.model['variables']]
 
-            # Transpose so each contrast gets a row of data instead of column
-            contrasts = block.get_contrasts([contrast['name']
-                                             for contrast in block.contrasts],
-                                            **ents)[0][0].T
-            # Add test indicator column
-            contrasts['type'] = [contrast['type']
-                                 for contrast in block.contrasts]
+                ent_string = '_'.join('{}-{}'.format(key, val)
+                                      for key, val in ents.items())
+                events_file = os.path.join(runtime.cwd,
+                                           '{}_events.h5'.format(ent_string))
+                confounds_file = os.path.join(runtime.cwd,
+                                              '{}_confounds.h5'.format(ent_string))
+                paradigm.to_hdf(events_file, key='events')
+                confounds[names].fillna(0).to_hdf(confounds_file, key='confounds')
+                info['events'] = events_file
+                info['confounds'] = confounds_file
+                info['repetition_time'] = TR
 
-            contrasts_file = os.path.join(runtime.cwd,
-                                          '{}_contrasts.h5'.format(ent_string))
-            contrasts.to_hdf(contrasts_file, key='contrasts')
+                # Transpose so each contrast gets a row of data instead of column
+                contrasts = block.get_contrasts([contrast['name']
+                                                 for contrast in block.contrasts],
+                                                **ents)[0][0].T
+                # Add test indicator column
+                contrasts['type'] = [contrast['type']
+                                     for contrast in block.contrasts]
 
-            entities.append(ents)
-            session_info.append(info)
-            contrast_info.append(contrasts_file)
+                contrasts_file = os.path.join(runtime.cwd,
+                                              '{}_contrasts.h5'.format(ent_string))
+                contrasts.to_hdf(contrasts_file, key='contrasts')
+
+                entities.append(ents)
+                session_info.append(info)
+                contrast_info.append(contrasts_file)
 
         runtime.analysis = analysis
 
