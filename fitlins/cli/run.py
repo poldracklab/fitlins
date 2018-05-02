9a7ebfb1f34df00f6538d97742937640310cb169
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-05-02 19:10:41 -0400

    RF: Create reports for default models

--- fitlins/cli/run.py
+++ fitlins/cli/run.py
@@ -15,7 +15,7 @@ import warnings
 from argparse import ArgumentParser
 from argparse import RawTextHelpFormatter
 
-from bids.analysis import Analysis
+from bids import grabbids as gb, analysis as ba
 
 from .. import __version__
 from ..workflows import init_fitlins_wf
@@ -143,13 +143,18 @@ def create_workflow(opts):
     except Exception:
         retcode = 1
 
-    analysis = Analysis(bids_dir, model)
-    report_dicts = parse_directory(deriv_dir, analysis)
+    layout = gb.BIDSLayout(bids_dir)
+    models = ba.auto_model(layout) if model == 'default' else [model]
+
     run_context = {'version': __version__,
                    'command': ' '.join(sys.argv),
                    'timestamp': time.strftime('%Y-%m-%d %H:%M:%S %z'),
                    }
-    write_report('unknown', report_dicts, run_context, deriv_dir)
+
+    for model in models:
+        analysis = ba.Analysis(layout, model=model)
+        report_dicts = parse_directory(deriv_dir, analysis)
+        write_report('unknown', report_dicts, run_context, deriv_dir)
 
     sys.exit(retcode)
 
