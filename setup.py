0c2fccea609ea64b97924034d2b93b4fb854ab3a
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-05-21 12:44:37 -0400

    FIX: Do not eval in setup

--- setup.py
+++ setup.py
@@ -5,45 +5,49 @@
 
 def main():
     """ Install entry-point """
-    from io import open
-    from os import path as op
-    from inspect import getfile, currentframe
     from setuptools import setup, find_packages
     import versioneer
-
-    this_path = op.dirname(op.abspath(getfile(currentframe())))
-
-    # Python 3: use a locals dictionary
-    # http://stackoverflow.com/a/1463370/6820620
-    ldict = locals()
-    # Get version and release info, which is all stored in fitlins/__about__.py
-    module_file = op.join(this_path, 'fitlins', '__about__.py')
-    with open(module_file) as infofile:
-        pythoncode = [line for line in infofile.readlines() if not line.strip().startswith('#')]
-        exec('\n'.join(pythoncode), globals(), ldict)
+    from fitlins.__about__ import (
+        __packagename__,
+        __version__,
+        __author__,
+        __email__,
+        __maintainer__,
+        __license__,
+        __description__,
+        __longdesc__,
+        __url__,
+        DOWNLOAD_URL,
+        CLASSIFIERS,
+        REQUIRES,
+        SETUP_REQUIRES,
+        LINKS_REQUIRES,
+        TESTS_REQUIRES,
+        EXTRA_REQUIRES,
+    )
 
     extensions = []
 
     setup(
-        name=ldict['__packagename__'],
-        version=ldict['__version__'],
+        name=__packagename__,
+        version=__version__,
         cmdclass=versioneer.get_cmdclass(),
-        description=ldict['__description__'],
-        long_description=ldict['__longdesc__'],
-        author=ldict['__author__'],
-        author_email=ldict['__email__'],
-        maintainer=ldict['__maintainer__'],
-        maintainer_email=ldict['__email__'],
-        url=ldict['__url__'],
-        license=ldict['__license__'],
-        classifiers=ldict['CLASSIFIERS'],
-        download_url=ldict['DOWNLOAD_URL'],
+        description=__description__,
+        long_description=__longdesc__,
+        author=__author__,
+        author_email=__email__,
+        maintainer=__maintainer__,
+        maintainer_email=__email__,
+        url=__url__,
+        license=__license__,
+        classifiers=CLASSIFIERS,
+        download_url=DOWNLOAD_URL,
         # Dependencies handling
-        setup_requires=ldict['SETUP_REQUIRES'],
-        install_requires=ldict['REQUIRES'],
-        tests_require=ldict['TESTS_REQUIRES'],
-        extras_require=ldict['EXTRA_REQUIRES'],
-        dependency_links=ldict['LINKS_REQUIRES'],
+        setup_requires=SETUP_REQUIRES,
+        install_requires=REQUIRES,
+        tests_require=TESTS_REQUIRES,
+        extras_require=EXTRA_REQUIRES,
+        dependency_links=LINKS_REQUIRES,
         package_data={'fitlins': ['data/fitlins.json', 'data/*.tpl']},
         entry_points={'console_scripts': ['fitlins=fitlins.cli.run:main']},
         packages=find_packages(exclude=("tests",)),
