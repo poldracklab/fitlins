d727c18a39f95a41ba6bb7f9ccea44da996f478d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-05-22 20:40:20 -0400

    FIX: Use fitlins/VERSION in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@
 
 def main():
     """ Install entry-point """
+    import os
     from setuptools import setup, find_packages
+    from inspect import getfile, currentframe
     import versioneer
     from fitlins.__about__ import (
         __packagename__,
@@ -26,12 +28,28 @@ def main():
         EXTRA_REQUIRES,
     )
 
+    pkg_data = {'fitlins': ['data/fitlins.json', 'data/*.tpl']}
+
+    root_dir = os.path.dirname(os.path.abspath(getfile(currentframe())))
+
+    version = None
+    cmdclass = {}
+    if os.path.isfile(os.path.join(root_dir, 'fitlins', 'VERSION')):
+        with open(os.path.join(root_dir, 'fitlins', 'VERSION')) as vfile:
+            version = vfile.readline().strip()
+        pkg_data['fitlins'].insert(0, 'VERSION')
+
+    if version is None:
+        import versioneer
+        version = versioneer.get_version()
+        cmdclass = versioneer.get_cmdclass()
+
     extensions = []
 
     setup(
         name=__packagename__,
-        version=__version__,
-        cmdclass=versioneer.get_cmdclass(),
+        version=version,
+        cmdclass=cmdclass,
         description=__description__,
         long_description=__longdesc__,
         author=__author__,
@@ -48,7 +66,7 @@ def main():
         tests_require=TESTS_REQUIRES,
         extras_require=EXTRA_REQUIRES,
         dependency_links=LINKS_REQUIRES,
-        package_data={'fitlins': ['data/fitlins.json', 'data/*.tpl']},
+        package_data=pkg_data,
         entry_points={'console_scripts': ['fitlins=fitlins.cli.run:main']},
         packages=find_packages(exclude=("tests",)),
         zip_safe=False,
