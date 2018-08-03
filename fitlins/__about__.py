2f5709be97b598b669285b14212b49643744993b
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-08-03 15:48:17 -0400

    PIN: nipy/nipype master

--- fitlins/__about__.py
+++ fitlins/__about__.py
@@ -47,8 +47,8 @@ REQUIRES = [
 ]
 
 LINKS_REQUIRES = [
-    'git+https://github.com/effigies/nipype.git@'
-    'c50839ee31494714cd93f2715bc3220131bc3215#egg=nipype',
+    'git+https://github.com/nipy/nipype.git@'
+    '91fbcce799e322483391b803862844f4ea56dfd9#egg=nipype',
     'git+https://github.com/INCF/pybids.git@'
     'a86a0e862e93001536bff28e4c2ba35906824d2c#egg=pybids',
     'git+https://github.com/nistats/nistats.git@'
