9e681a5443a1100d3b96e4cccd3d295cf49eac7f
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-08-03 11:53:27 -0400

    RF: Remove pre-nipype run_model

--- fitlins/cli/run.py
+++ fitlins/cli/run.py
@@ -166,13 +166,9 @@ def run_fitlins(argv=None):
         include_pattern=opts.include, exclude_pattern=opts.exclude
         )
 
+    retcode = 0
     try:
         fitlins_wf.run(**plugin_settings)
-        if model != 'default':
-            retcode = run_model(model, opts.space, level, opts.bids_dir, preproc_dir,
-                                deriv_dir)
-        else:
-            retcode = 0
     except Exception:
         retcode = 1
 
@@ -190,18 +186,6 @@ def run_fitlins(argv=None):
         write_report('unknown', report_dicts, run_context, deriv_dir)
 
     return retcode
-
-
-def run_model(model, space, target_level, bids_dir, preproc_dir, deriv_dir):
-    analysis = init(model, bids_dir, preproc_dir)
-    if analysis.blocks[0].level == target_level:
-        return 0
-    for block in analysis.blocks[1:]:
-        second_level(analysis, block, space, deriv_dir)
-        if block.level == target_level:
-            break
-
-    return 0
 
 
 def main():
