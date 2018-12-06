e1314913bcf0bee73af5d84b99ebc829cefcc23e
delavega4 <aleph4@gmail.com>
2018-12-06 17:53:01 -0600

    Merge remove get_ev

--- fitlins/viz/reports.py
+++ fitlins/viz/reports.py
@@ -29,8 +29,9 @@ def deroot(val, root):
 
 def parse_directory(deriv_dir, work_dir, analysis):
     fl_layout = bids.BIDSLayout(
-        (deriv_dir, ['bids', 'derivatives',
-                     pkgr.resource_filename('fitlins', 'data/fitlins.json')]))
+        deriv_dir,
+        config=['bids', 'derivatives',
+                pkgr.resource_filename('fitlins', 'data/fitlins.json')])
     wd_layout = bids.BIDSLayout(str(Path(work_dir) / 'reportlets' / 'fitlins'))
     contrast_svgs = fl_layout.get(extensions='.svg', type='contrasts')
 
