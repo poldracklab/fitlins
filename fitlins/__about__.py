e6eb55289cfb6bbec0f10b05e92b2ba86d728a85
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-02-20 11:54:20 -0500

    MNT: Move fully to pyproject.toml+setup.cfg

--- fitlins/__about__.py
+++ fitlins/__about__.py
@@ -10,7 +10,7 @@ __version__ = get_versions()['version']
 del get_versions
 
 __author__ = 'The CRN developers'
-__copyright__ = ('Copyright 2018, Center for Reproducible Neuroscience, '
+__copyright__ = ('Copyright 2019, Center for Reproducible Neuroscience, '
                  'Stanford University')
 __credits__ = ['Christopher J. Markiewicz', 'Chris Gorgolewski',
                'Russell A. Poldrack']
@@ -20,49 +20,8 @@ __email__ = 'crn.poldracklab@gmail.com'
 __status__ = 'Prototype'
 __url__ = 'https://github.com/poldracklab/fitlins'
 __packagename__ = 'fitlins'
-__description__ = 'Fit Linear Models to fMRI Data'
-__longdesc__ = ''
+__description__ = 'Fit Linear Models to BIDS Datasets'
 
 DOWNLOAD_URL = (
     'https://github.com/poldracklab/{name}/archive/{ver}.tar.gz'.format(
         name=__packagename__, ver=__version__))
-
-
-SETUP_REQUIRES = [
-    'setuptools>=27.0',
-]
-
-REQUIRES = [
-    'nibabel>=2.0',
-    'nipype>=1.1.6',
-    'seaborn>=0.7.1',
-    'numpy>=1.11',
-    'nilearn>=0.4.0',
-    'pandas>=0.19',
-    'tables>=3.2.1',
-    'nistats>=0.0.1b0',
-    'pybids>=0.8.0',
-    'jinja2',
-]
-
-LINKS_REQUIRES = [
-]
-
-TESTS_REQUIRES = [
-]
-
-EXTRA_REQUIRES = {
-    'tests': TESTS_REQUIRES,
-    'duecredit': ['duecredit'],
-}
-
-# Enable a handle to install all extra dependencies at once
-EXTRA_REQUIRES['all'] = [val for _, val in list(EXTRA_REQUIRES.items())]
-CLASSIFIERS = [
-    'Development Status :: 2 - Pre-Alpha',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-]
--- setup.py
+++ setup.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ fitlins setup script """
+from setuptools import setup
 
 
 def main():
     """ Install entry-point """
     import os
-    from setuptools import setup, find_packages
     from inspect import getfile, currentframe
-    import versioneer
     from fitlins.__about__ import (
-        __packagename__,
-        __version__,
-        __author__,
-        __email__,
-        __maintainer__,
-        __license__,
-        __description__,
-        __longdesc__,
-        __url__,
         DOWNLOAD_URL,
-        CLASSIFIERS,
-        REQUIRES,
-        SETUP_REQUIRES,
-        LINKS_REQUIRES,
-        TESTS_REQUIRES,
-        EXTRA_REQUIRES,
     )
 
     pkg_data = {'fitlins': ['data/fitlins.json', 'data/*.tpl']}
@@ -44,33 +28,10 @@ def main():
         version = versioneer.get_version()
         cmdclass = versioneer.get_cmdclass()
 
-    extensions = []
-
     setup(
-        name=__packagename__,
         version=version,
         cmdclass=cmdclass,
-        description=__description__,
-        long_description=__longdesc__,
-        author=__author__,
-        author_email=__email__,
-        maintainer=__maintainer__,
-        maintainer_email=__email__,
-        url=__url__,
-        license=__license__,
-        classifiers=CLASSIFIERS,
         download_url=DOWNLOAD_URL,
-        # Dependencies handling
-        setup_requires=SETUP_REQUIRES,
-        install_requires=REQUIRES,
-        tests_require=TESTS_REQUIRES,
-        extras_require=EXTRA_REQUIRES,
-        dependency_links=LINKS_REQUIRES,
-        package_data=pkg_data,
-        entry_points={'console_scripts': ['fitlins=fitlins.cli.run:main']},
-        packages=find_packages(exclude=("tests",)),
-        zip_safe=False,
-        ext_modules=extensions
     )
 
 
