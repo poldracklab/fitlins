d3d0d983b18ae155c91a7345579d7c94019c3fdd
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-09-26 09:38:14 -0400

    FIX: Delay interface imports to avoid premature etelemetry

--- fitlins/workflows/base.py
+++ fitlins/workflows/base.py
@@ -1,12 +1,5 @@
 from pathlib import Path
 import warnings
-# from nipype.interfaces import fsl
-from ..interfaces.bids import (
-    ModelSpecLoader, LoadBIDSModel, BIDSSelect, BIDSDataSink)
-from ..interfaces.nistats import FirstLevelModel, SecondLevelModel
-from ..interfaces.visualizations import (
-    DesignPlot, DesignCorrelationPlot, ContrastMatrixPlot, GlassBrainPlot)
-from ..interfaces.utils import MergeAll, CollateWithMetadata
 
 
 def init_fitlins_wf(bids_dir, derivatives, out_dir, analysis_level, space,
@@ -16,6 +9,12 @@ def init_fitlins_wf(bids_dir, derivatives, out_dir, analysis_level, space,
                     base_dir=None, name='fitlins_wf'):
     from nipype.pipeline import engine as pe
     from nipype.interfaces import utility as niu
+    from ..interfaces.bids import (
+        ModelSpecLoader, LoadBIDSModel, BIDSSelect, BIDSDataSink)
+    from ..interfaces.nistats import FirstLevelModel, SecondLevelModel
+    from ..interfaces.visualizations import (
+        DesignPlot, DesignCorrelationPlot, ContrastMatrixPlot, GlassBrainPlot)
+    from ..interfaces.utils import MergeAll, CollateWithMetadata
     wf = pe.Workflow(name=name, base_dir=base_dir)
 
     # Find the appropriate model file(s)
