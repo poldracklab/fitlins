c64135efa65a059c96e00ee6d98513a62a8b8215
delavega4 <aleph4@gmail.com>
2019-12-11 16:04:04 -0600

    Use layout.root to indicate where file could not be found

--- fitlins/interfaces/bids.py
+++ fitlins/interfaces/bids.py
@@ -398,13 +398,13 @@ class BIDSSelect(SimpleInterface):
 
             if len(bold_file) == 0:
                 raise FileNotFoundError(
-                    "Could not find BOLD file with entities {}"
-                    "".format(selectors))
+                    "Could not find BOLD file in {} with entities {}"
+                    "".format(layout.root, selectors))
             elif len(bold_file) > 1:
                 raise ValueError(
-                    "Non-unique BOLD file with entities {}.\n"
+                    "Non-unique BOLD file in {} with entities {}.\n"
                     "Matches:\n\t{}"
-                    "".format(selectors,
+                    "".format(layout.root, selectors,
                               "\n\t".join(
                                   '{} ({})'.format(
                                       f.path,
