303ad4e9d92b6e8bbad41ccb8773012510324a74
delavega4 <aleph4@gmail.com>
2020-02-26 15:10:47 -0600

    Skeleton diagonostics module

---
+++ fitlins/viz/diagnostics.py
@@ -0,0 +1,9 @@
+class Diagnostics:
+    """ Warnings and plots generated from a first-level design matrix """
+
+    @staticmethod
+    def _check_covariance(dm):
+        pass
+
+    def run(dm):
+        pass
