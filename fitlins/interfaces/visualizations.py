c01bc284b85aebb0e30c5fe08acb1a368d5967f3
Satrajit Ghosh <satra@mit.edu>
2020-06-18 16:42:45 -0400

    ref: interfaces to functions

--- fitlins/interfaces/visualizations.py
+++ fitlins/interfaces/visualizations.py
@@ -1,50 +1,17 @@
+import os
+
 import pandas as pd
 import nibabel as nb
 from nilearn import plotting as nlp
 import nistats as nis
-import nistats.reporting  # noqa: F401
-
-from nipype.interfaces.base import (
-    SimpleInterface, BaseInterfaceInputSpec, TraitedSpec,
-    File, traits, isdefined
-    )
 from nipype.utils.filemanip import fname_presuffix, split_filename
 
 from ..viz import plot_and_save, plot_corr_matrix, plot_contrast_matrix
 
 
-class VisualizationInputSpec(BaseInterfaceInputSpec):
-    data = File(mandatory=True, desc='Data file to visualize')
-    image_type = traits.Enum('svg', 'png', mandatory=True)
+def visualization(data, image_type, visualize):
 
-
-class VisualizationOutputSpec(TraitedSpec):
-    figure = File(desc='Visualization')
-
-
-class Visualization(SimpleInterface):
-    input_spec = VisualizationInputSpec
-    output_spec = VisualizationOutputSpec
-
-    def _run_interface(self, runtime):
-        import matplotlib
-        matplotlib.use('Agg')
-        import seaborn as sns
-        from matplotlib import pyplot as plt
-        sns.set_style('white')
-        plt.rcParams['svg.fonttype'] = 'none'
-        plt.rcParams['image.interpolation'] = 'nearest'
-
-        data = self._load_data(self.inputs.data)
-        out_name = fname_presuffix(self.inputs.data,
-                                   suffix='.' + self.inputs.image_type,
-                                   newpath=runtime.cwd,
-                                   use_ext=False)
-        self._visualize(data, out_name)
-        self._results['figure'] = out_name
-        return runtime
-
-    def _load_data(self, fname):
+    def _load_data(fname):
         _, _, ext = split_filename(fname)
         if ext == '.tsv':
             return pd.read_table(fname, index_col=0)
@@ -52,24 +19,35 @@ class Visualization(SimpleInterface):
             return nb.load(fname)
         raise ValueError("Unknown file type!")
 
+    import matplotlib
+    matplotlib.use('Agg')
+    import seaborn as sns
+    from matplotlib import pyplot as plt
+    sns.set_style('white')
+    plt.rcParams['svg.fonttype'] = 'none'
+    plt.rcParams['image.interpolation'] = 'nearest'
 
-class DesignPlot(Visualization):
-    def _visualize(self, data, out_name):
+    data = _load_data(data)
+    out_name = fname_presuffix(data,
+                               suffix='.' + image_type,
+                               newpath=os.getcwd(),
+                               use_ext=False)
+    visualize(data, out_name)
+    return out_name
+
+
+def DesignPlot(data, image_type):
+    def _visualize(data, out_name):
         from matplotlib import pyplot as plt
         plt.set_cmap('viridis')
         plot_and_save(out_name, nis.reporting.plot_design_matrix, data)
+    return visualization(data, image_type, _visualize)
 
 
-class DesignCorrelationPlotInputSpec(VisualizationInputSpec):
-    contrast_info = traits.List(traits.Dict)
-
-
-class DesignCorrelationPlot(Visualization):
-    input_spec = DesignCorrelationPlotInputSpec
-
-    def _visualize(self, data, out_name):
+def DesignCorrelationPlot(data, image_type, contrast_info):
+    def _visualize(data, out_name):
         contrast_matrix = pd.DataFrame({c['name']: c['weights'][0]
-                                        for c in self.inputs.contrast_info})
+                                        for c in contrast_info})
         all_cols = list(data.columns)
         evs = set(contrast_matrix.index)
         if set(contrast_matrix.index) != all_cols[:len(evs)]:
@@ -79,46 +57,33 @@ class DesignCorrelationPlot(Visualization):
         plot_and_save(out_name, plot_corr_matrix,
                       data.drop(columns='constant').corr(),
                       len(evs))
+    return visualization(data, image_type, _visualize)
 
 
-class ContrastMatrixPlotInputSpec(VisualizationInputSpec):
-    contrast_info = traits.List(traits.Dict)
-    orientation = traits.Enum('horizontal', 'vertical', usedefault=True,
-                              desc='Display orientation of contrast matrix')
-
-
-class ContrastMatrixPlot(Visualization):
-    input_spec = ContrastMatrixPlotInputSpec
-
-    def _visualize(self, data, out_name):
+def ContrastMatrixPlot(data, image_type, contrast_info, orientation):
+    def _visualize(data, out_name):
         contrast_matrix = pd.DataFrame({c['name']: c['weights'][0]
-                                        for c in self.inputs.contrast_info},
+                                        for c in contrast_info},
                                        index=data.columns)
         contrast_matrix.fillna(value=0, inplace=True)
         if 'constant' in contrast_matrix.index:
             contrast_matrix = contrast_matrix.drop(index='constant')
         plot_and_save(out_name, plot_contrast_matrix, contrast_matrix,
                       ornt=self.inputs.orientation)
+    return visualization(data, image_type, _visualize)
 
-class GlassBrainPlotInputSpec(VisualizationInputSpec):
-    threshold = traits.Enum('auto', None, traits.Float(), usedefault=True)
-    vmax = traits.Float()
-    colormap = traits.Str('bwr', usedefault=True)
 
-class GlassBrainPlot(Visualization):
-    input_spec = GlassBrainPlotInputSpec
-
-    def _visualize(self, data, out_name):
+def GlassBrainPlot(data, image_type, threshold, vmax, colormap):
+    def _visualize(data, out_name):
         import numpy as np
-        vmax = self.inputs.vmax
-        if not isdefined(vmax):
-            vmax = None
+        if vmax is None:
             abs_data = np.abs(data.get_fdata())
             pctile99 = np.percentile(abs_data, 99.99)
             if abs_data.max() - pctile99 > 10:
                 vmax = pctile99
         nlp.plot_glass_brain(data, colorbar=True, plot_abs=False,
                              display_mode='lyrz', axes=None,
-                             vmax=vmax, threshold=self.inputs.threshold,
-                             cmap=self.inputs.colormap,
+                             vmax=vmax, threshold=threshold,
+                             cmap=colormap,
                              output_file=out_name)
+    return visualization(data, image_type, _visualize)
