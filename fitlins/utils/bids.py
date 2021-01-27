80e0c32d514a3c791a365c38fa9bb4175fee6f71
delavega4 <aleph4@gmail.com>
2021-01-27 15:43:08 -0600

    writeout estimator to derivative description

--- fitlins/utils/bids.py
+++ fitlins/utils/bids.py
@@ -107,7 +107,7 @@ def collect_participants(layout, participant_label=None, strict=False):
     return found_label
 
 
-def write_derivative_description(bids_dir, deriv_dir):
+def write_derivative_description(bids_dir, deriv_dir, estimator):
     from fitlins import __version__
 
     desc = {
@@ -117,6 +117,7 @@ def write_derivative_description(bids_dir, deriv_dir):
             'Name': 'FitLins',
             'Version': __version__,
             'CodeURL': 'https://github.com/poldracklab/fitlins',
+            'Estimator': estimator,
             },
         'CodeURL': 'https://github.com/poldracklab/fitlins',
         'HowToAcknowledge': 'https://github.com/poldracklab/fitlins',
