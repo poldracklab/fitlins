1e1333992d61b48d2878c3bfaee074e85dd6226d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2021-12-02 13:38:22 -0500

    ENH: Make to_alphanum more robust, add tests

--- fitlins/utils/strings.py
+++ fitlins/utils/strings.py
@@ -1,8 +1,32 @@
+import re
+
+
 def snake_to_camel(string):
     words = string.split('_')
     return words[0] + ''.join(word.title() for word in words[1:])
 
 
 def to_alphanum(string):
-    string = string.replace('.', '_').replace('-', '_')
-    return snake_to_camel(string)
+    """ Convert string to alphanumeric
+
+    Replaces all other characters with underscores and then converts to camelCase
+
+    Examples
+    --------
+
+    >>> to_alphanum('abc123')
+    'abc123'
+    >>> to_alphanum('a four word phrase')
+    'aFourWordPhrase'
+    >>> to_alphanum('hyphen-separated')
+    'hyphenSeparated'
+    >>> to_alphanum('object.attribute')
+    'objectAttribute'
+    >>> to_alphanum('array[index]')
+    'arrayIndex'
+    >>> to_alphanum('array[0]')
+    'array0'
+    >>> to_alphanum('snake_case')
+    'snakeCase'
+    """
+    return snake_to_camel(re.sub("[^a-zA-Z0-9]", "_", string))
