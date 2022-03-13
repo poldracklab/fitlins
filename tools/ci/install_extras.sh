9f5c9282b2f34217d252eca90b99841e8aa71024
Shashank Bansal <shashankbansal56@gmail.com>
2022-03-13 15:00:18 -0700

    CI: Use conda env setup for pytests and parameterize over python v3.7-3.10

---
+++ tools/ci/install_extras.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo Installing dependencies
+
+source tools/ci/env.sh
+
+set -eu
+
+# Required variables
+echo EXTRA_PIP_FLAGS = $EXTRA_PIP_FLAGS
+echo CHECK_TYPE = $CHECK_TYPE
+
+set -x
+
+if [ -n "$EXTRA_PIP_FLAGS" ]; then
+    EXTRA_PIP_FLAGS=${!EXTRA_PIP_FLAGS}
+fi
+
+pip install $EXTRA_PIP_FLAGS "fitlins[$CHECK_TYPE]"
+
+set +eux
+
+echo Done installing dependencies
