af6a5c65e39e4041a84d19cefe430afb36d73a48
Alejandro de la Vega <aleph4@gmail.com>
2022-04-06 23:11:32 -0500

    Update fitlins/viz/reports.py
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>

--- fitlins/viz/reports.py
+++ fitlins/viz/reports.py
@@ -142,9 +142,7 @@ def write_full_report(report_dict, run_context, deriv_dir):
 
     tpl = env.get_template('data/full_report.tpl')
 
-    model_name = snake_to_camel(report_dict['model']['name'])
-    if model_name == "":
-        model_name = "untitled"
+    model_name = snake_to_camel(report_dict['model'].get('name') or "untitled")
     target_file = op.join(
         deriv_dir, fl_layout.build_path({'model': model_name}, PATH_PATTERNS, validate=False)
     )
